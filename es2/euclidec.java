1f6450e33231670038ebfffb5fcd24bdcc27d3e8
Davvo06 <Davideale2021@gmail.com>
2024-10-04 07:21:25 +0000

    update

---
+++ es2/euclidec.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class euclidec {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Inserisci il primo numero (a): ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Inserisci il secondo numero (b): ");
+        int b = scanner.nextInt();
+        
+        int mcd = calcolaMCD(a, b);
+        
+        System.out.println("Il MCD tra " + a + " e " + b + " è: " + mcd);
+    }
+    
+    public static int calcolaMCD(int a, int b) {
+        while (b != 0) {
+            int resto = a % b;
+            a = b;
+            b = resto;
+        }
+        return a;
+    }
+}
